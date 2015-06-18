d0cc0ee803b6a19a457fc1c561a57f9063a88829
Babette Stam <babettestam@gmail.com>
2015-06-18 11:03:02 +0200

    Add OPEN Rotterdam header

--- wp/wp-content/themes/kijkinjewijk/header.php
+++ wp/wp-content/themes/kijkinjewijk/header.php
@@ -7,6 +7,147 @@
 </head>
 <body <?php body_class( $class ); ?>>
 <div id="wrapper">
-<!-- <div id="header">
-	<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu' ) ); ?>
-</div> -->
+<div id="top">
+    <div class="container">
+        <a href="./OPEN Rotterdam_files/OPEN Rotterdam.html" class="logo">
+        </a>
+
+<ul class="navigatie">
+        <li><p>NIEUWS</p>
+            <ul style="display: none;">
+                    <li>
+                                                            <a href="http://www.openrotterdam.nl/headlines/nieuws/overzicht?604" title="Headlines">
+                            Headlines                        </a>
+                                                </li>
+                </ul>
+        </li>
+        <li><p>AUDIO</p>
+            <ul style="display: none;">
+                    <li>
+                                                            <a href="http://www.openrotterdam.nl/radiogemist/overzicht" title="AUDIO ITEMS">
+                            AUDIO ITEMS                        </a>
+                                                </li>
+                </ul>
+        </li>
+        <li><p>TELEVISIE</p>
+            <ul style="display: none;">
+                    <li>
+                                                            <a href="http://www.openrotterdam.nl/tvgemist/archief" title="UITZENDING GEMIST">
+                            UITZENDING GEMIST                        </a>
+                                                </li>
+                    <li>
+                                                            <a href="http://www.openrotterdam.nl/livestream" title="LIVESTREAM">
+                            LIVESTREAM                        </a>
+                                                </li>
+                    <li>
+                                    <a href="http://www.openrotterdam.nl/programmering/content/page?879" title="PROGRAMMERING">
+                        PROGRAMMERING                    </a>
+                            </li>
+                </ul>
+        </li>
+        <li><p>OVER ONS</p>
+            <ul style="display: none;">
+                    <li>
+                                    <a href="http://www.openrotterdam.nl/frequenties/content/page?881" title="Frequenties">
+                        Frequenties                    </a>
+                            </li>
+                    <li>
+                                    <a href="http://www.openrotterdam.nl/adverteren/content/page?877" title="Adverteren">
+                        Adverteren                    </a>
+                            </li>
+                    <li>
+                                    <a href="http://www.openrotterdam.nl/contact/content/page?880" title="Contact">
+                        Contact                    </a>
+                            </li>
+                    <li>
+                                                            <a href="http://www.openrotterdam.nl/contact" title="Contactformulier">
+                            Contactformulier                        </a>
+                                                </li>
+                    <li>
+                                                            <a href="http://www.openrotterdam.nl/uploaduwbijdrage" title="Zelf bijdragen">
+                            Zelf bijdragen                        </a>
+                                                </li>
+                </ul>
+        </li>
+        <li><p>OPEN Podium</p>
+            <ul style="display: none;">
+                    <li>
+                                    <a href="http://www.openrotterdam.nl/over-open-podium/content/page?1876" title="OVER OPEN PODIUM">
+                        OVER OPEN PODIUM                    </a>
+                            </li>
+                    <li>
+                                    <a href="http://www.openrotterdam.nl/frequenties/content/page?886" title="FREQUENTIES">
+                        FREQUENTIES                    </a>
+                            </li>
+                    <li>
+                                                            <a href="http://www.openrotterdam.nl/tvgidsop" title="PROGRAMMERING">
+                            PROGRAMMERING                        </a>
+                                                </li>
+                    <li>
+                                                            <a href="http://www.openrotterdam.nl/tvgemistop" title="UITZENDING GEMIST">
+                            UITZENDING GEMIST                        </a>
+                                                </li>
+                    <li>
+                                                            <a href="http://www.openrotterdam.nl/open_podium_tv_livestream" title="LIVESTREAM">
+                            LIVESTREAM                        </a>
+                                                </li>
+                </ul>
+        </li>
+        <li><p>ACTIES</p>
+            <ul style="display: none;">
+                    <li>
+                                    <a href="http://www.openrotterdam.nl/actie-wij-zijn-open/content/page?936" title="ACTIE: WIJ ZIJN OPEN">
+                        ACTIE: WIJ ZIJN OPEN                    </a>
+                            </li>
+                    <li>
+                                    <a href="http://www.openrotterdam.nl/rotterdam-de-allermooiste-rotstad/content/page?1383" title="Rotterdam de allermooiste rotstad">
+                        Rotterdam de allermooiste rotstad                    </a>
+                            </li>
+                    <li>
+                                    <a href="http://www.openrotterdam.nl/race-the-tube/content/page?1400" title="Race The Tube">
+                        Race The Tube                    </a>
+                            </li>
+                    <li>
+                                    <a href="http://www.openrotterdam.nl/winacties-open-rotterdam/content/page?1609" title="WINACTIES OPEN ROTTERDAM">
+                        WINACTIES OPEN ROTTERDAM                    </a>
+                            </li>
+                    <li>
+                                    <a href="http://www.openrotterdam.nl/het-leukste-huisdier-van-rotterdam/content/page?1771" title="Het leukste huisdier van Rotterdam">
+                        Het leukste huisdier van Rotterdam                    </a>
+                            </li>
+                    <li>
+                                    <a href="http://www.openrotterdam.nl/waar-is-open/content/page?1774" title="Waar is OPEN?">
+                        Waar is OPEN?                    </a>
+                            </li>
+                    <li>
+                                    <a href="http://www.openrotterdam.nl/koningsdag-2015-rotterdam/content/page?1788" title="Koningsdag 2015 Rotterdam">
+                        Koningsdag 2015 Rotterdam                    </a>
+                            </li>
+                    <li>
+                                    <a href="http://www.openrotterdam.nl/de-lokale-omroep-awards-2015/content/page?1810" title="De Lokale Omroep Awards 2015">
+                        De Lokale Omroep Awards 2015                    </a>
+                            </li>
+                    <li>
+                                    <a href="http://www.openrotterdam.nl/summerschool-2015/content/page?1841" title="SUMMERSCHOOL 2015">
+                        SUMMERSCHOOL 2015                    </a>
+                            </li>
+                    <li>
+                                    <a href="http://www.openrotterdam.nl/rotterdream/content/page?1855" title="ROTTERDREAM">
+                        ROTTERDREAM                    </a>
+                            </li>
+                </ul>
+        </li>
+        <li><p>PARTNERS</p>
+            <ul style="display: none;">
+                    <li>
+                                    <a href="http://www.openrotterdam.nl/m-de-koning-autolease/content/page?1794" title="M. de Koning Autolease ">
+                        M. de Koning Autolease                     </a>
+                            </li>
+                </ul>
+        </li>
+    <li class="clear"></li>
+</ul>
+
+        <div class="clear"></div>
+    </div>
+</div>
